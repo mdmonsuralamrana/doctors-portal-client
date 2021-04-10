9aee431a1c06e903184f46a84b32f34fed3fd98f
mdmonsuralamrana <monsuralam7262@gmail.com>
2021-04-11 02:39:57 +0600

    Home page created

---
+++ src/Components/Home/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ServiceDetail = ({service}) => {
+    return (
+        <div className="col-md-4 text-center">
+            <img style={{height: '50px'}} src={service.img} />
+            <h5 className="mt-3 mb-3">{service.name}</h5>
+            <p className="text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+    );
+};
+
+export default ServiceDetail
